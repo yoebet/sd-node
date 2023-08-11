a13cf2c28a8cdab1b4ee309d469200d9397029ad
yobett <yobett@hxr.com>
2023-08-12 01:38:32 +0800

    docs

--- javascript/subpath-patch.js
+++ javascript/subpath-patch.js
@@ -9,3 +9,11 @@ window.fetch = function () {
     }
     return ofetch(args)
 }
+
+
+setTimeout(function () {
+    docs_link = document.querySelector('#footer > div:nth-child(1) > a:nth-child(1)')
+    if (docs_link && docs_link.getAttribute('href') === '/docs') {
+        docs_link.setAttribute('href', 'docs')
+    }
+}, 3000)
